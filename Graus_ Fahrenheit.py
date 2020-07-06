5bf17467710db7d40a74e255b1537bd6cdf64f69
Diogo O. Souza <60483946+gitrexe@users.noreply.github.com>
2020-07-06 09:46:26 -0300

    Update Graus_ Fahrenheit.py

--- Graus_ Fahrenheit.py
+++ Graus_ Fahrenheit.py
@@ -1,4 +1,4 @@
-c = float(input('Informe a temperatura em C: '))
+c = float(input('Enter the temperature in °C: '))
 f = ((9 * c)/ 5) + 32
 print('A temperatura de {} é °C correspondente a {} °F'.format(c, f))
 
